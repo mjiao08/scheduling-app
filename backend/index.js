a289a2f6c416e0a5e88587f20e46d193ef047bb6
mjiao08 <masonjiao591806@gmail.com>
2025-02-18 02:32:19 -0600

    Set up backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,48 @@
 require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+const { Pool } = require('pg');
+
+const PORT = process.env.PORT || 5000;
+
+// app.use(cors({
+//     origin: ''     
+// }));
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+pool.connect((err, client, release) => {
+    if (err) {
+        console.error('Error acquiring client from pool', err.stack);
+        return;
+    }
+
+    console.log('Successfully connected to PostgreSQL');
+    release();   
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to your scheduling app API')
+});
+
+app.get('/api/schedules', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM schedules');
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error executing query', err.stack);
+        res.status(500).json({ error: 'Database query failed' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
